597a3050694d295bdd624b793cd3bc99713f03ee
Mustafa Jaweed <mustafa.jaweed@syniti.com>
2023-08-07 01:27:36 +0530

    My first python changes

---
+++ projects/bandGenerator.py
@@ -0,0 +1,7 @@
+# print("Hello") #print a string
+#Take input from user
+# name = input("What is your name? \n")
+# print("Hello " + name)
+# print("Hello " + input("What is your name? \n"))
+print("Band generator")
+print("The name of your band could be " + input("Which city were you born\n") + " " + input("What is your pet's name\n"))
